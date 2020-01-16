01a298d5360f9e6d85700e9eff688e709f9ce4fc
Alex Hong <alexhong681@gmail.com>
2020-01-15 21:36:27 -0800

    modified mongo uri

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 //mongo connection
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI_LOCAL, { useNewUrlParser: true} );
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true} );
 mongoose.connection.on('error', (error) => {
     console.error('Database connection error:', error);
 });
